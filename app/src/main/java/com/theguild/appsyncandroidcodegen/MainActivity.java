6b84af8a4e092546be2070106512fc0ebad95787
Dotan Simha <dotansimha@gmail.com>
2019-09-25 22:56:18 +0300

    basic appsync with amplify

--- app/src/main/java/com/theguild/appsyncandroidcodegen/MainActivity.java
+++ app/src/main/java/com/theguild/appsyncandroidcodegen/MainActivity.java
@@ -3,12 +3,48 @@ package com.theguild.appsyncandroidcodegen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.amazonaws.mobile.config.AWSConfiguration;
+import com.amazonaws.mobileconnectors.appsync.AWSAppSyncClient;
+import com.amazonaws.mobileconnectors.appsync.fetcher.AppSyncResponseFetchers;
+import com.apollographql.apollo.GraphQLCall;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
+import com.amazonaws.amplify.generated.graphql.ListProductsQuery;
+
+import javax.annotation.Nonnull;
 
 public class MainActivity extends AppCompatActivity {
+    private AWSAppSyncClient mAWSAppSyncClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAWSAppSyncClient = AWSAppSyncClient.builder()
+                .context(getApplicationContext())
+                .awsConfiguration(new AWSConfiguration(getApplicationContext()))
+                .build();
+
+        this.runQuery();
     }
+
+    public void runQuery() {
+        mAWSAppSyncClient.query(ListProductsQuery.builder().build())
+                .responseFetcher(AppSyncResponseFetchers.CACHE_AND_NETWORK)
+                .enqueue(todosCallback);
+    }
+
+    private GraphQLCall.Callback<ListProductsQuery.Data> todosCallback = new GraphQLCall.Callback<ListProductsQuery.Data>() {
+        @Override
+        public void onResponse(@Nonnull Response<ListProductsQuery.Data> response) {
+            Log.i("Results", response.data().listProducts().items().toString());
+        }
+
+        @Override
+        public void onFailure(@Nonnull ApolloException e) {
+            Log.e("ERROR", e.toString());
+        }
+    };
 }
